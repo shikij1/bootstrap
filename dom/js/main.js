e13ae1f290d7f756f9980261ee02e6b253278bf6
Shikiji <56910224+shikij1@users.noreply.github.com>
2020-01-23 21:47:00 +0900

    confirm utilities

---
+++ dom/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+{
+  document.title = "fjdisajfiajiwejiaje";
+  document.querySelector("h1").textContent = "H1!";
+  document.querySelectorAll("li").forEach(li => {
+    li.textContent = "fdijsajf";
+  });
+}
